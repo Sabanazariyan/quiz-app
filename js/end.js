ce89f6e969a209e6f84efc8a4e26d9afa3aa71b9
saba <mahdaneparsa@gmail.com>
2025-01-18 12:02:11 +0330

    completet end html styles and save user name and score and highscore in local storage

---
+++ js/end.js
@@ -0,0 +1,25 @@
+const score = JSON.parse(localStorage.getItem("score"));
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const scoreElement = document.querySelector("p");
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+
+scoreElement.innerText = score;
+
+const savedHandler = () => {
+  if (!input.value || !score) {
+    alert("Invalid Username Or Score");
+  } else {
+    const finalScore = { name: input.value, score: score };
+    highScores.push(finalScore);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(10);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    localStorage.removeItem("scores");
+    window.location.assign("/");
+  }
+};
+
+button.addEventListener("click", savedHandler);
