f82b4ee7b2fb6d259762d7289f711fd9e77ed6e0
saba <mahdaneparsa@gmail.com>
2025-01-18 10:12:02 +0330

    show correct or incorrect answer when click on a button and show score for correct answer

--- js/game.js
+++ js/game.js
@@ -3,18 +3,21 @@ const loader = document.getElementById("loader");
 const container = document.getElementById("container");
 const questionText = document.getElementById("question-text");
 const answerList = document.querySelectorAll(".answer-text");
+const scoreText = document.getElementById("score");
 
+const CORRECT_BONUS = 10;
 const URL =
   "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 let formattedData = null;
 let questionIndex = 0;
 let correctAnswer = null;
+let score = 0;
+let isAccepted = true;
 
 const fetchData = async () => {
   const response = await fetch(URL);
   const json = await response.json();
   formattedData = formatData(json.results);
-  console.log(formattedData);
   start();
 };
 
@@ -28,15 +31,31 @@ const showQuestion = () => {
   const { question, answers, correctAnswerIndex } =
     formattedData[questionIndex];
   correctAnswer = correctAnswerIndex;
+  console.log(correctAnswer);
   questionText.innerText = question;
   answerList.forEach((button, index) => {
+    //ایندکس برای درست نشستن سوال ها در باتن ها
     button.innerText = answers[index];
   });
 };
 
-const checkAnswer = () => {};
+const checkAnswer = (event, index) => {
+  //این دو خط برای این است که ما فقط یکبار حق انتخاب جواب داشته باشیم
+  if (!isAccepted) return;
+  isAccepted = false;
+  const isCorrect = index === correctAnswer ? true : false;
+  if (isCorrect) {
+    event.target.classList.add("correct");
+    score += CORRECT_BONUS;
+    scoreText.innerText = score;
+  } else {
+    event.target.classList.add("incorrect");
+    answerList[correctAnswer].classList.add("correct");
+  }
+};
 
 window.addEventListener("load", fetchData);
+//ایندکس برای مقایسه باتن کلیک شده و ایندکس جواب درست سوال
 answerList.forEach((button, index) => {
-  button.addEventListener("click", checkAnswer);
+  button.addEventListener("click", (event) => checkAnswer(event, index));
 });
